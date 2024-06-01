f8bfa4bde92009632fac1327e0dfd3584f7fe7c4
Abhishek Kumar <avi.kr16@gmail.com>
2024-06-01 11:40:46 +0530

    complete push

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,13 +1,16 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:faker_app_flutter_firebase/src/screens/ui_%20auth_providers.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authProviders = [EmailAuthProvider()];
+    final authProviders = ref.watch(authProvidersProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign in'),
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker/faker.dart' hide Job;
+import 'package:faker_app_flutter_firebase/src/data/firestore_repository.dart';
+import 'package:faker_app_flutter_firebase/src/data/job.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -15,12 +20,74 @@ class HomeScreen extends ConsumerWidget {
           onPressed: () => context.goNamed(AppRoute.profile.name),
         )
       ]),
+      body: JobsListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          debugPrint('Not implemented');
+          final user = ref.read(firebaseAuthProvider).currentUser;
+          final faker = Faker();
+          final title = faker.job.title();
+          final company = faker.company.name();
+          ref.read(firestoreRepositoryProvider).addJob(
+                user!.uid,
+                title,
+                company,
+              );
         },
       ),
+    );
+  }
+}
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    final user = ref.watch(firebaseAuthProvider).currentUser;
+    return FirestoreListView<Job>(
+      query: firestoreRepository.jobsQuery(user!.uid),
+      errorBuilder: (context, error, stackTrace) => Center(
+        child: Text(error.toString()),
+      ),
+      emptyBuilder: (context) => const Center(
+        child: Text('No data'),
+      ),
+      itemBuilder: (BuildContext context, QueryDocumentSnapshot<Job> doc) {
+        final job = doc.data();
+        return Dismissible(
+          key: Key(doc.id),
+          background: ColoredBox(color: Colors.red),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            final user = ref.read(firebaseAuthProvider).currentUser;
+            ref.read(firestoreRepositoryProvider).deleteJob(user!.uid, doc.id);
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            trailing: job.createdAt != null
+                ? Text(
+                    job.createdAt.toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  )
+                : null,
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
+              final title = faker.job.title();
+              final company = faker.company.name();
+              ref.read(firestoreRepositoryProvider).updateJob(
+                    user!.uid,
+                    doc.id,
+                    title,
+                    company,
+                  );
+            },
+          ),
+        );
+      },
     );
   }
 }
